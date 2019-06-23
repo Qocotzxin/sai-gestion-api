b047ecbee45676d6b2e49a12db247612deaf1516
Qocotzxin <cristianmetchebarne@gmail.com>
2019-06-23 16:10:04 -0300

    CORS actualizado

--- src/server/middlewares/cors.ts
+++ src/server/middlewares/cors.ts
@@ -10,9 +10,10 @@ import { NextFunction, Response, Request } from 'express';
  */
 export const enableCors = (req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   next();
 };
