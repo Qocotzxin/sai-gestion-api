eb5fd425df18de856b803e5de719fd5d79362651
cristian-etchebarne <cristian.etchebarne@globallogic.com>
2019-10-21 23:26:54 -0300

    Endpoint de verificacion para email y username creado

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,5 +9,7 @@ const app = express();
 
 app.use(require('./weather'));
 app.use(require('./login'));
+app.use(require('./verification'));
+app.use(require('./signup'));
 
 module.exports = app;
---
+++ src/server/routes/verification.ts
@@ -0,0 +1,31 @@
+/*!
+ * SAI - Copyright 2019
+ *  Cristian Etchebarne
+ */
+
+import express, { Request, Response } from 'express';
+import { User } from '../models/user';
+import { ERROR_TYPE, setErrorResponse } from '../config/error';
+const UserModel = require("../models/user");
+
+const app = express();
+
+/**
+ * Endpoint: /v1/verify/:field
+ * Verifies if email/username exists.
+ */
+app.get('/v1/verify', (req: Request, res: Response) => {
+  const email = req.query.email;
+  const username = req.query.username;
+
+  UserModel.findOne(email ? { email } : { username }, (error: Error, user: User) => {
+    if (error) return setErrorResponse(res, ERROR_TYPE.INTERNAL);
+
+    if (user) return res.status(200).json({ ok: true, data: { exists: true }, message: 'A user already exists associated with this information.' });
+
+    return res.status(200).json({ ok: true, data: { exists: false }, message: 'No user associated with this information.' });
+  });
+
+});
+
+module.exports = app;
