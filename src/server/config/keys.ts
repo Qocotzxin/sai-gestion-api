a25f66c7ed1e33b3f9886ddf6be6a1d43317cb2d
cristian-etchebarne <cristian.etchebarne@globallogic.com>
2019-10-29 21:38:53 -0300

    Api keys ocultas

---
+++ src/server/config/keys.ts
@@ -0,0 +1,11 @@
+/*!
+ * SAI - Copyright 2019
+ *  Cristian Etchebarne
+ */
+
+export const KEYS = {
+  SG: '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',
+  IP_INFO: '022e2e7adeddef',
+  SEED: 'pchowypbldepymipkmzzyjsc',
+  OPEN_WEATHER: 'dd38ea8bc2ba3a1e81d195a9d4a7be24'
+}
